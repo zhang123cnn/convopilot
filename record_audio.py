e8e6118b90a19f4366754f9f00d428b9b32be682
Xiaomeng Zhang <zhang123cnn@gmail.com>
2023-08-06 22:52:24 +0100

    Saving transcription to target file

--- record_audio.py
+++ record_audio.py
@@ -1,3 +1,4 @@
+import argparse
 import pyaudio
 import time
 import threading
@@ -5,13 +6,13 @@ import queue
 import whisper
 import numpy as np
 
-CHUNK_DURATION = 10
+CHUNK_DURATION = 20
 RATE = 16000
 CHANNELS = 1
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 audio_queue = queue.Queue()
-model = whisper.load_model("small")
+model = whisper.load_model("medium")
 
 #TODO
 # 1. Write transcription data to file.
@@ -29,19 +30,31 @@ def record_audio(q):
             chunk_data += data
         q.put(chunk_data)
 
-def save_audio(q):
+def transcribe_audio(q, outputfile):
     while True:
         chunk_data = q.get()
         data = np.frombuffer(chunk_data, np.int16).flatten().astype(np.float32) / 32768.0
         result = model.transcribe(data)
+        # open local file to append result into it
+        with open(outputfile, "a") as f:
+           f.write(result['text'])
+
         print(result['text'])
+        
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--output_file", "-o", type=str, default="./transcription.txt", help="file to save the outputs")
+
+    args = parser.parse_args().__dict__
+    output_file: str = args.pop("output_file")
+
     record_thread = threading.Thread(target=record_audio, args=(audio_queue,))
-    save_thread = threading.Thread(target=save_audio, args=(audio_queue,))
+    transcribe_thread = threading.Thread(target=transcribe_audio, args=(audio_queue, output_file))
 
     record_thread.start()
-    save_thread.start()
+    transcribe_thread.start()
 
     try:
         while True:
