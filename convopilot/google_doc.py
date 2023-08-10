96426da1761272e33042293b172cbc0c709a875d
Xiaomeng Zhang <zhang123cnn@gmail.com>
2023-08-10 23:57:35 +0100

    add google doc integration

---
+++ convopilot/google_doc.py
@@ -0,0 +1,68 @@
+import pickle
+import os.path
+import json
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def get_creds():
+    credentials_text = os.environ.get("GOOGLE_DOC_CREDENTIALS")
+    credentials = json.loads(credentials_text)
+    # Load the credentials
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no valid credentials available, prompt the user to log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_config(
+                credentials,
+                ['https://www.googleapis.com/auth/documents', 
+                 'https://www.googleapis.com/auth/drive', 
+                 'https://www.googleapis.com/auth/drive.file'])
+            creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+    return creds
+
+creds = get_creds()
+
+def create_doc(title, folder_id):
+    service = build('docs', 'v1', credentials=creds)
+
+    document = service.documents().create(body={'title': title}).execute()
+
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    file_id = document['documentId']
+    drive_service.files().update(fileId=file_id, addParents=folder_id, fields='id, parents').execute()
+
+    return document
+
+
+# Upload a string to the Google Doc
+def insert_text(doc_id, text):
+    print(text)
+    service = build('docs', 'v1', credentials=creds)
+    requests = [
+        {
+            'insertText': {
+                'location': {
+                    'index': 1,
+                },
+                'text': text + '\n'
+            }
+        }
+    ]
+    try:
+        result = service.documents().batchUpdate(
+            documentId=doc_id, body={'requests': requests}).execute()
+        return result
+    except HttpError as error:
+        print(f'An error occurred: {error}')
--- convopilot/record_audio.py
+++ convopilot/record_audio.py
@@ -6,11 +6,12 @@ import queue
 import whisper
 import numpy as np
 
-from convopilot.llm_models import get_llm_model
-
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from convopilot.llm_models import get_llm_model
+from convopilot import google_doc
 
 CHUNK_DURATION = 30
 RATE = 16000
@@ -22,12 +23,14 @@ transcription_queue = queue.Queue()
 model = whisper.load_model("medium")
 should_stop = False
 
+
 def record_audio(q):
     p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    input=True, frames_per_buffer=CHUNK)
 
     print("Recording... Press Ctrl+C to stop.")
-    
+
     while not should_stop:
         chunk_data = b''
         for _ in range(0, int(RATE / CHUNK * CHUNK_DURATION)):
@@ -40,13 +43,15 @@ def record_audio(q):
     q.put(None)
     print("Stopping recording.")
 
-def transcribe_audio(q, tq, outputfile):
+
+def transcribe_audio(q, tq, outputfile, gdocument):
     transcription_data = ""
     while True:
         chunk_data = q.get()
         if (chunk_data is None):
             break
-        data = np.frombuffer(chunk_data, np.int16).flatten().astype(np.float32) / 32768.0
+        data = np.frombuffer(chunk_data, np.int16).flatten().astype(
+            np.float32) / 32768.0
         result = model.transcribe(data)
         # open local file to append result into it
         if outputfile == "stdout":
@@ -58,11 +63,14 @@ def transcribe_audio(q, tq, outputfile):
         transcription_data += result['text']
         tq.put(transcription_data)
 
+    google_doc.insert_text(gdocument['documentId'], transcription_data)
+    google_doc.insert_text(gdocument['documentId'], "Transcription:")
+
     tq.put(None)
     print("Stopping transcription.")
 
 
-def generate_llm_insights(tq, context, llm_model, llm_prompt): 
+def generate_llm_insights(tq, context, llm_model, llm_prompt, gdocument):
     model = get_llm_model(llm_model)
     previous_response = ""
     while True:
@@ -81,16 +89,30 @@ def generate_llm_insights(tq, context, llm_model, llm_prompt):
         previous_response = response
         print(response)
 
+    google_doc.insert_text(gdocument['documentId'], previous_response)
+    google_doc.insert_text(gdocument['documentId'], llm_prompt)
     print("Stopping llm generation.")
-    
-def start(output_file, llm_model, llm_prompt):
+
+
+def start(output_file, llm_model, llm_prompt, googledoc_metadata):
+    document = None
+    if googledoc_metadata is not None:
+        if googledoc_metadata['name'] == "" or googledoc_metadata['folder'] == "":
+            print("Please provide a valid google doc name and folder")
+            return
+
+        document = google_doc.create_doc(googledoc_metadata['name'], googledoc_metadata['folder'])
+        print(f"Created google doc at https://docs.google.com/document/d/{document['documentId']}/edit")
+
     if llm_model != "none":
         context = input("Please enter some context for the conversation: ")
-        llm_thread = threading.Thread(target=generate_llm_insights, args=(transcription_queue, context, llm_model, llm_prompt))
+        llm_thread = threading.Thread(target=generate_llm_insights, args=(
+            transcription_queue, context, llm_model, llm_prompt, document))
         llm_thread.start()
 
     record_thread = threading.Thread(target=record_audio, args=(audio_queue,))
-    transcribe_thread = threading.Thread(target=transcribe_audio, args=(audio_queue, transcription_queue, output_file))
+    transcribe_thread = threading.Thread(target=transcribe_audio, args=(
+        audio_queue, transcription_queue, output_file, document))
 
     record_thread.start()
     transcribe_thread.start()
@@ -101,7 +123,8 @@ def start(output_file, llm_model, llm_prompt):
     except KeyboardInterrupt:
         global should_stop
         should_stop = True
-    
+
+
     record_thread.join()
     transcribe_thread.join()
     if llm_model != "none":
@@ -111,18 +134,39 @@ def start(output_file, llm_model, llm_prompt):
 
 
 def cli():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--output_file", "-o", type=str, default="stdout", help="file to save the outputs")
-    parser.add_argument("--llm_model", "-m", type=str, default="none", help="llm model to use for conversation analysis")
-    parser.add_argument("--llm_prompt", "-p", type=str, default="Could you summarize the top insights from the conversation below", help="prompt used for real time conversation analysis")
+    parser.add_argument("--output_file", "-o", type=str,
+                        default="stdout", help="file to save the outputs")
+    parser.add_argument("--llm_model", "-m", type=str, default="none",
+                        help="llm model to use for conversation analysis")
+    parser.add_argument("--llm_prompt", "-p", type=str, default="Could you summarize the top insights from the conversation below",
+                        help="prompt used for real time conversation analysis")
+    parser.add_argument("--googledoc", "-t", type=bool,
+                        default=False, help="use google doc to save the outputs")
+    parser.add_argument("--googledocname", "-n", type=str, default="",
+                        help="name of the google doc to save the outputs")
+    parser.add_argument("--googledocfolder", "-f", type=str, default="",
+                        help="folder of the google doc to save the outputs")
 
     args = parser.parse_args().__dict__
     output_file: str = args.pop("output_file")
     llm_model: str = args.pop("llm_model")
     llm_prompt: str = args.pop("llm_prompt")
+    googledoc: bool = args.pop("googledoc")
+    googledocname: str = args.pop("googledocname")
+    googledocfolder: str = args.pop("googledocfolder")
 
-    start(output_file, llm_model, llm_prompt)
+    googledoc_metadata = None
+    if googledoc:
+        googledoc_metadata = {
+            "name": googledocname,
+            "folder": googledocfolder
+        }
+
+    start(output_file, llm_model, llm_prompt, googledoc_metadata)
+
 
 if __name__ == "__main__":
     cli()
