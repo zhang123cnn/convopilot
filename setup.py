01521bf688e83fdcd3789085a1757ad4edfe26d4
Xiaomeng Zhang <zhang123cnn@gmail.com>
2023-08-13 23:46:32 +0100

    new pypi version after refactor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='convopilot',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(include=['convopilot']),
     license="MIT",
     author="Xiaomeng Zhang",
