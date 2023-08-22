3b139dd26eae85931aa044549fd9fcde4eec127c
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2023-08-22 15:36:28 +0100

    remove devmenu

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -40,10 +40,6 @@ if (process.env.NODE_ENV === 'production') {
 const isDebug =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
-if (isDebug) {
-  require('electron-debug')();
-}
-
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
