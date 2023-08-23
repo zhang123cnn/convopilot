f7231343e93556f7a548a08355cddce8af4d19bd
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2023-08-23 22:58:20 +0100

    fix errors in app renderer

--- app/src/renderer/App.tsx
+++ app/src/renderer/App.tsx
@@ -2,6 +2,43 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
+function startServer() {
+  const data = {
+    llm_model: 'gpt-3.5',
+    llm_prompt:
+      'Could you summarize the top insights from the conversation in bullet points?',
+    llm_context: 'no context',
+  };
+
+  fetch('http://127.0.0.1:5555/start', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(() => {
+      console.log('HTTP request sent successfully');
+      return null;
+    })
+    .catch((error) => {
+      console.error('Error sending HTTP request:', error);
+    });
+}
+
+function stopServer() {
+  fetch('http://127.0.0.1:5555/stop', {
+    method: 'POST',
+  })
+    .then(() => {
+      console.log('Server stopped successfully');
+      return null;
+    })
+    .catch((error) => {
+      console.error('Error stopping server:', error);
+    });
+}
+
 function Hello() {
   return (
     <div>
@@ -11,6 +48,7 @@ function Hello() {
       <h1 style={{ textAlign: 'center' }}>Convo Pilot</h1>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <button
+          type="button"
           onClick={() => {
             startServer();
           }}
@@ -18,6 +56,7 @@ function Hello() {
           Start
         </button>
         <button
+          type="button"
           style={{ marginLeft: '10px' }}
           onClick={() => {
             stopServer();
@@ -28,40 +67,6 @@ function Hello() {
       </div>
     </div>
   );
-}
-
-function startServer() {
-  const data = {
-    llm_model: "gpt-3.5",
-    llm_prompt: "Could you summarize the top insights from the conversation in bullet points?",
-    llm_context: "no context"
-  };
-
-  fetch("http://127.0.0.1:5555/start", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(response => {
-      console.log("HTTP request sent successfully");
-    })
-    .catch(error => {
-      console.error("Error sending HTTP request:", error);
-    });
-}
-
-function stopServer() {
-  fetch('http://127.0.0.1:5555/stop', {
-    method: 'POST'
-  })
-    .then(response => {
-      console.log('Server stopped successfully');
-    })
-    .catch(error => {
-      console.error('Error stopping server:', error);
-    });
 }
 
 export default function App() {
