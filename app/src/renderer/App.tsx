f796c06a8fc8e075ecd753d8fd8ce612a4ddd5e3
Xiaomeng Zhang <zhang123cnn@gmail.com>
2023-08-26 09:02:35 +0100

    remove unused functions

--- app/src/renderer/App.tsx
+++ app/src/renderer/App.tsx
@@ -6,43 +6,6 @@ const io = require('socket.io-client');
 
 const socket = io('http://127.0.0.1:5555');
 
-function startServer() {
-  const data = {
-    llm_model: 'gpt-3.5',
-    llm_prompt:
-      'Could you summarize the top insights from the conversation in bullet points?',
-    llm_context: 'no context',
-  };
-
-  fetch('http://127.0.0.1:5555/start', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then(() => {
-      console.log('HTTP request sent successfully');
-      return null;
-    })
-    .catch((error) => {
-      console.error('Error sending HTTP request:', error);
-    });
-}
-
-function stopServer() {
-  fetch('http://127.0.0.1:5555/stop', {
-    method: 'POST',
-  })
-    .then(() => {
-      console.log('Server stopped successfully');
-      return null;
-    })
-    .catch((error) => {
-      console.error('Error stopping server:', error);
-    });
-}
-
 function Hello() {
   return (
     <div>
