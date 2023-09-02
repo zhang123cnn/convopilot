2ced882577972e3877d631fe8c55798e424b2884
Xiaomeng Zhang <xiaomeng@fb.com>
2023-09-02 08:53:16 +0100

    fix requirements.txt and bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='convopilot',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(include=['convopilot']),
     license="MIT",
     author="Xiaomeng Zhang",
