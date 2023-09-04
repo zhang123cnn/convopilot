5f4e93735e795ee80a668eaee7825d052502a061
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2023-09-04 23:45:47 +0100

    modify file_writer to match api

--- convopilot/file_writer.py
+++ convopilot/file_writer.py
@@ -1,18 +1,15 @@
+import os
+
 class FileWriter:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+
     def write(self, filename, data, mode='w'):
         try:
-            with open(filename, mode) as file:
+            full_path = os.path.join(self.output_dir, filename)
+            with open(full_path, mode) as file:
                 file.write(data)
-            print(f"Data has been written to {filename} using mode '{mode}'.")
+            print(f"Data has been written to {full_path} using mode '{mode}'.")
         except Exception as e:
             print(f"An error occurred: {e}")
 
-if __name__ == "__main__":
-    try:
-        writer = FileWriter()
-        filename = "transcription.txt"
-        transcription_data = "This is some transcription data."
-
-        writer.write(filename, transcription_data)
-    except Exception as e:
-        print(f"An error occurred: {e}")
