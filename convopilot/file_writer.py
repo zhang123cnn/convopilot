4bb1f5cea4b27e6af0d33f2473077ce4195522db
Xiaomeng Zhang <zhang123cnn@gmail.com>
2023-09-07 08:12:53 +0100

    update the file writer for its original version

--- convopilot/file_writer.py
+++ convopilot/file_writer.py
@@ -1,15 +1,22 @@
+import datetime
 import os
+
 
 class FileWriter:
     def __init__(self, output_dir):
         self.output_dir = output_dir
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
 
-    def write(self, filename, data, mode='w'):
-        try:
-            full_path = os.path.join(self.output_dir, filename)
-            with open(full_path, mode) as file:
-                file.write(data)
-            print(f"Data has been written to {full_path} using mode '{mode}'.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    def write(self, file_name, content):
+        file_path = os.path.join(self.output_dir, file_name)
 
+        # check if file already exists, then add datetime into the filename to avoid duplication
+        if os.path.exists(file_path):
+            file_name, file_ext = os.path.splitext(file_name)
+            now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            file_name = f"{file_name}_{now}{file_ext}"
+            file_path = os.path.join(self.output_dir, file_name)
+
+        with open(file_path, "a") as f:
+            f.write(content)
