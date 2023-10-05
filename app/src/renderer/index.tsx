d6d4607409dc273a00b692680fb21528d35e6ab8
Xiaomeng Zhang <zhang123cnn@gmail.com>
2023-10-05 23:52:23 +0100

    fix minor

--- app/src/renderer/index.tsx
+++ app/src/renderer/index.tsx
@@ -3,6 +3,7 @@ import App from './App';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
+
 root.render(<App />);
 
 // calling IPC exposed from preload script
@@ -11,7 +12,3 @@ window.electron.ipcRenderer.once('ipc-example', (arg) => {
   console.log(arg);
 });
 window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
-
-window.electron.ipcRenderer.once('load-data', (event: any, data: any) => {
-  console.log('load data', data);
-});
